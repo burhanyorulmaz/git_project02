3abf12f9e650bb212728c111d60aa19f99b58411
Burhan Yorulmaz <Burhanyorulmaz02@gmail.com>
2020-03-15 21:42:04 +0100

    third commit from  local branch

--- src/gitPackageOne/GitDay3.java
+++ src/gitPackageOne/GitDay3.java
@@ -10,6 +10,7 @@ public static void main(String[]args) {
 	System.out.println("My first changes on local master");
 	System.out.println("This is my feature branch");
 	System.out.println("My second push from local feature branch");
+	System.out.println("My third push from local feature branch");
 	
 	
 }
